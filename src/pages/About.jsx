3c349e9b76ce8205d1676defe0f0321db16ad496
Seif abdelaty <seifeldeen2011@gmail.com>
2026-01-21 01:33:56 +0200

    initial commit

---
+++ src/pages/About.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const About = () => {
+ return (
+  <div className='container mx-auto px-4 py-8'>
+   <h1 className='text-3xl font-bold text-gray-600 mb-8'>About Us</h1>
+   <p className='text-gray-600'>Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Donec odio. Quisque volutpat mattis eros. Nullam malesuada erat ut turpis.</p>
+  </div>
+ );
+};
+
+export default About;
