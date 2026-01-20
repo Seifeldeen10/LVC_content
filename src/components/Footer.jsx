515e3834057fa33ba5483e844452946281300b1d
Seif abdelaty <seifeldeen2011@gmail.com>
2026-01-21 01:10:48 +0200

    initial commit

---
+++ src/components/Footer.jsx
@@ -0,0 +1,1 @@
+import React from 'react'; function Footer() { return (<footer className='container mx-auto'> <div className='grid grid-cols-1 md:grid-cols-3 gap-6'></div> </footer>); } export default Footer;
