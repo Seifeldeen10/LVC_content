a72179f546ac80a6df3085fbeacab9aa42d9001b
Seif abdelaty <seifeldeen2011@gmail.com>
2026-01-21 17:12:49 +0200

    initial commit

---
+++ src/components/Footer.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export const Footer = () => {
+  return (
+    <footer className='p-5 bg-blue-500'>
+      <div className='text-center text-white'>&copy; 2022 LVC Corporate. All rights reserved.</div>
+    </footer>
+  );
+}
