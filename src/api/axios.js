f76d87491ccd437e64213f8de33a0e7d4280bb09
Seifeldeen10 <seifeldeen2011@gmail.com>
2026-01-21 15:00:28 +0200

    Remove all files

--- src/api/axios.js
+++
@@ -1,9 +0,0 @@
-import axios from 'axios';
-
-const instance = axios.create({
-  baseURL: 'https://your-api-endpoint.com',
-  timeout: 3000,
-  headers: {'Content-Type': 'application/json'}
-});
-
-export default instance;
