b0e7d93ce3fbdbfc33cb35fa858897582ed3b167
Seifeldeen10 <seifeldeen2011@gmail.com>
2026-01-21 17:11:39 +0200

    Remove all files

--- src/components/Footer.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-export const Footer = () => {
-  return (
-    <footer className='bg-gray-900 text-white p-5 text-center'>
-      <p>2022 Copyright</p>
-    </footer>
-  );
-};
--- src/pages/InvestmentStrategy.jsx
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-
-function InvestmentStrategy() {
-  return <div>Investment Strategy</div>
-}
-
-export default InvestmentStrategy
