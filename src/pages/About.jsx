f26f02fa0e66f8720708c4a4c47d8670b1544b55
Seif abdelaty <seifeldeen2011@gmail.com>
2026-01-21 16:32:39 +0200

    initial commit

---
+++ src/pages/About.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import Navbar from '../components/Navbar'
+
+const About = () => {
+  return (
+    <div>
+      <Navbar />
+      <section className='container mx-auto h-full flex justify-center items-center'>
+        <h1 className='text-2xl'>About LVC Corporate</h1>
+      </section>
+    </div>
+  )
+}
+
+export default About
