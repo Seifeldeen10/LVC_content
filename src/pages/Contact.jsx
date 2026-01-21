efc49ad0dcf61bb616622cc5856dab31aba8da98
Seif abdelaty <seifeldeen2011@gmail.com>
2026-01-21 16:32:40 +0200

    initial commit

---
+++ src/pages/Contact.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import Navbar from '../components/Navbar'
+
+const Contact = () => {
+  return (
+    <div>
+      <Navbar />
+      <section className='container mx-auto h-full flex justify-center items-center'>
+        <h1 className='text-2xl'>Contact LVC Corporate</h1>
+      </section>
+    </div>
+  )
+}
+
+export default Contact
